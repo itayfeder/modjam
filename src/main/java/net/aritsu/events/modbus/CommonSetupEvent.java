0a03c7054a392570a364f391fad210472b65475a
Subaraki <robbeenaxel@hotmail.com>
2021-08-20 12:00:23 +0200

    Remove example mod. Added base Mod Class. Added two base setup events.

---
+++ src/main/java/net/aritsu/events/modbus/CommonSetupEvent.java
@@ -0,0 +1,17 @@
+package net.aritsu.events.modbus;
+
+import net.aritsu.mod.AritsuMod;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+
+@Mod.EventBusSubscriber(modid = AritsuMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class CommonSetupEvent {
+
+    @SubscribeEvent
+    public static void setupClient(FMLCommonSetupEvent event){
+
+    }
+}
---
+++ src/main/java/net/aritsu/mod/AritsuMod.java
@@ -0,0 +1,13 @@
+package net.aritsu.mod;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(AritsuMod.MODID)
+public class AritsuMod {
+
+    public static final String MODID = "aritsumods";
+    public static final Logger LOGGER = LogManager.getLogger();
+
+}
