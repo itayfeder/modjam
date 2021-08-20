b1d1b0ed30c06c24024be410228441434f55a30e
Subaraki <robbeenaxel@hotmail.com>
2021-08-20 12:32:34 +0200

    Add configuration files

---
+++ src/main/java/net/aritsu/util/ConfigData.java
@@ -0,0 +1,60 @@
+package net.aritsu.util;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+import org.apache.commons.lang3.tuple.Pair;
+
+public class ConfigData {
+
+    public static final ServerConfig SERVER;
+    public static final ForgeConfigSpec SERVER_SPEC;
+    public static final ClientConfig CLIENT;
+    public static final ForgeConfigSpec CLIENT_SPEC;
+
+    public static boolean exampleClientBoolean = false;
+    public static int exampleServerInteger = 0;
+
+    static {
+        final Pair<ServerConfig, ForgeConfigSpec> specPair = new ForgeConfigSpec.Builder().configure(ServerConfig::new);
+        SERVER_SPEC = specPair.getRight();
+        SERVER = specPair.getLeft();
+    }
+
+    static {
+        final Pair<ClientConfig, ForgeConfigSpec> specPair = new ForgeConfigSpec.Builder().configure(ClientConfig::new);
+        CLIENT_SPEC = specPair.getRight();
+        CLIENT = specPair.getLeft();
+    }
+
+    public static void refreshClient() {
+        exampleClientBoolean = CLIENT.booleanSample.get();
+    }
+
+    public static void refreshServer() {
+        exampleServerInteger = SERVER.intValue.get();
+
+    }
+
+    public static class ClientConfig {
+
+        public final ForgeConfigSpec.BooleanValue booleanSample;
+
+        ClientConfig(ForgeConfigSpec.Builder builder) {
+
+            builder.push("general");
+            booleanSample = builder.comment("Pick true to show a larger model in the wardrobe screen").translation("translate.show.size").define("bigger_model", true);
+            builder.pop();
+
+        }
+    }
+
+    public static class ServerConfig {
+        public final ForgeConfigSpec.IntValue intValue;
+
+        ServerConfig(ForgeConfigSpec.Builder builder) {
+
+            builder.push("general");
+            intValue = builder.comment("a sample int value").translation("int.value.sample").defineInRange("sample_value", 0, -10, 100);
+            builder.pop();
+        }
+    }
+}
