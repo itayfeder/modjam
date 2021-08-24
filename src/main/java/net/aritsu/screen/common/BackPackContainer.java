ef537a0574715fb6e8171d3a9c3c7197ac8bedc5
Subaraki <robbeenaxel@hotmail.com>
2021-08-24 12:06:39 +0200

    Fix quickMoveStack crashing game. Shift click in backpack implemented. Demo for specific slots available : use glass bottle to shift click it into the flask's specific slot

--- src/main/java/net/aritsu/screen/common/BackPackContainer.java
+++ src/main/java/net/aritsu/screen/common/BackPackContainer.java
@@ -7,25 +7,30 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.MenuConstructor;
 import net.minecraft.world.inventory.Slot;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraftforge.items.ItemStackHandler;
 
 public class BackPackContainer extends AbstractContainerMenu {
 
     /*******************************************************/
 
+    final ItemStackHandler backpackInventory;
+
     public BackPackContainer(int id, Inventory playerInventory, ItemStackHandler inventory) {
         super(AritsuContainers.BACKPACK_CONTAINER_TYPE.get(), id);
+        backpackInventory = inventory;
 
-            this.addSlot(new FlaskSlot(inventory, 0, 44, 20));
-            this.addSlot(new EnderSlot(inventory, 1, 62, 20));
+        this.addSlot(new FlaskSlot(inventory, 0, 44, 20));
+        this.addSlot(new EnderSlot(inventory, 1, 62, 20));
 
-            this.addSlot(new SleepingBagSlot(inventory, 2, 44, 38));
-            this.addSlot(new TentSlot(inventory, 3, 62, 38));
+        this.addSlot(new SleepingBagSlot(inventory, 2, 44, 38));
+        this.addSlot(new TentSlot(inventory, 3, 62, 38));
 
-            for (int y = 0; y < 2; ++y) {
-                for (int x = 0; x < 3; ++x) {
-                    this.addSlot(new BackPackSlot(inventory, 1 + 3 * (1 + y) + x, 80 + x * 18, 20 + y * 18));
-                }
+        for (int y = 0; y < 2; ++y) {
+            for (int x = 0; x < 3; ++x) {
+                this.addSlot(new BackPackSlot(inventory, 1 + 3 * (1 + y) + x, 80 + x * 18, 20 + y * 18));
+            }
         }
 
         ///////////////////////ADD PLAYER INVENTORY///////////////////////////////
@@ -56,5 +61,39 @@ public class BackPackContainer extends AbstractContainerMenu {
     @Override
     public boolean stillValid(Player player) {
         return true;
+    }
+
+    @Override
+    public ItemStack quickMoveStack(Player player, int slotnumber) {
+        ItemStack copy = ItemStack.EMPTY;
+        Slot clicked = this.getSlot(slotnumber);
+        if (clicked != null && clicked.hasItem()) {
+            ItemStack slotItem = clicked.getItem();
+            copy = slotItem.copy();
+
+            //shift clicked inside backpack
+            if (slotnumber < backpackInventory.getSlots()) {
+                //move to player inventory
+                if (!this.moveItemStackTo(slotItem, backpackInventory.getSlots(), this.slots.size(), true)) {
+                    return ItemStack.EMPTY;
+                }
+            } else {
+                //check for specific item
+                //TODO add all specific items
+                if (slotItem.getItem().equals(Items.GLASS_BOTTLE)) {
+                    //move to specific item slot
+                    if (!this.moveItemStackTo(slotItem, 0, 1, false)) {
+                        return ItemStack.EMPTY;
+                    }
+                    //move to extra slots
+                } else if (!this.moveItemStackTo(slotItem, 4, backpackInventory.getSlots(), true)) {
+                    return ItemStack.EMPTY;
+                }
+            }
+            if (slotItem.isEmpty())
+                clicked.set(ItemStack.EMPTY);
+            else clicked.setChanged();
+        }
+        return copy;
     }
 }
