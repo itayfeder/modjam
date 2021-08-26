4c916db6e1751b90dcaf5b29d28399d3400be515
Subaraki <robbeenaxel@hotmail.com>
2021-08-27 00:51:15 +0200

    Fix Server Crash. Colors are Client Only Fix terrible variable name

--- src/main/java/net/aritsu/events/modbus/ColorEvents.java
+++ src/main/java/net/aritsu/events/modbus/ColorEvents.java
@@ -3,25 +3,20 @@ package net.aritsu.events.modbus;
 import net.aritsu.mod.AritsuMod;
 import net.aritsu.registry.AritsuItems;
 import net.minecraft.client.color.item.ItemColor;
-import net.minecraft.client.color.item.ItemColors;
 import net.minecraft.world.item.alchemy.PotionUtils;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.ColorHandlerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-@Mod.EventBusSubscriber(modid = AritsuMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+@Mod.EventBusSubscriber(modid = AritsuMod.MODID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ColorEvents {
     @SubscribeEvent
     public static void registerColorHandlers(ColorHandlerEvent.Item event) {
-
-        registerItemColorHandlers(event.getItemColors());
-    }
-
-    private static void registerItemColorHandlers(final ItemColors itemColors) {
-        final ItemColor FlaskColorHandler = (p_92699_, p_92700_) -> {
-            return p_92700_ > 0 ? -1 : PotionUtils.getColor(p_92699_);
+        final ItemColor flaskColorHandler = (anInt, itemStack) -> {
+            return itemStack > 0 ? -1 : PotionUtils.getColor(anInt);
         };
 
-        itemColors.register(FlaskColorHandler, AritsuItems.FLASK.get());
+        event.getItemColors().register(flaskColorHandler, AritsuItems.FLASK.get());
     }
 }
